6366cd3cd457e7a98f59bbf8eb6e015de5cc2541
woosung2 <jeongws3240@gmail.com>
2020-12-09 00:29:43 +0900

    app.py before comment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_jwt_extended import *
 from pymongo import MongoClient
+import datetime
 
 app = Flask(__name__)
 
@@ -42,9 +43,10 @@ def login():
     print(user_pw)
 
     ## *** find_one 시에 아무것도 없을 때의 데이터 형태 알아야함 ***
-    user = db.users.find_one( {'user_id' : user_id},{'user_pwd' : user_pw})
+    user = db.articles.find_one( {'user_id' : user_id},{'user_pwd' : user_pw})
     if user is None:
         return jsonify({'login' : False})
+
 
     access_token = create_access_token(identity = user_id, expires_delta = False)
     refresh_token = create_refresh_token(identity = user_id)
@@ -58,6 +60,101 @@ def login():
     print(access_token)
     print(refresh_token)
     return resp, 200
+
+# 실명게시판 글쓰기버튼 작동
+@app.route('/article/known/write')
+def modify_articles():
+    return render_template('modify.html', article_is_secret = False)
+
+# 익명게시판 글쓰기버튼
+@app.route('/article/unknown/write')
+def modify_articles():
+    return render_template('modify.html', article_is_secret = True)
+
+# 실명게시판 글쓰기 완료버튼 작동
+@app.route('/article/known/post', methods=['POST'])
+def post_articles():
+    article_title = request.form['title_input']
+    article_content = request.form['content_input']
+    now = datetime.datetime.now()
+    article_created_at = now.today() # 시간
+    article_modified_at = now.today()  # 시간
+    article_view = 0
+    article_like = 0
+    article_is_secret = False
+    article_user_id = get_jwt_identity()
+
+    db.articles.insert_one({'article_title' : article_title, 'article_content' : article_content, 'article_created_at' : article_created_at,
+                         'article_modified_at' : article_modified_at,'article_view' : article_view, 'article_like' : article_like, 'article_is_secret' :article_is_secret,
+                         'article_user_id' : article_user_id})
+
+    return redirect('/article/known')
+
+
+# 실명게시판 상세페이지 (GET ? POST ?)
+@app.route('/article/<article_id>', methods=['GET'])
+def read_articles(article_id):
+    article = db.articles.find_one({'_id' : article_id})
+    user_id = get_jwt_identity()
+
+    return render_template('read.html', article = article ,user_id = user_id)
+
+
+# 수정 버튼을 누르면
+@app.route('/article/<article_id>/modify', methods=['PUT'])
+def modify_articles(article_id):
+    article = db.articles.find_one({'_id' : article_id})
+    return render_template('modify.html', article = article)
+
+# 수정완료 버튼을 누르면
+@app.route('/article/<article_id>/modify_pro')
+def modify_pro(article_id):
+    article_title = request.form['title_input']
+    article_content = request.form['content_input']
+    now = datetime.datetime.now()
+    article_modified_at = now.today()
+
+
+    db.articles.update_one({'_id': article_id }, {'$set':{'article_title' : article_title,
+                                                          'article_content' : article_content,'article_modified_at' : article_modified_at}})
+
+    return redirect('/article/known')
+
+
+# 삭제
+@app.route('/article/<article_id>/delete', methods=['DELETE'])
+def delete_articles(article_id):
+    db.articles.delete_one({'_id' : article_id})
+    return  redirect('/article/known')
+
+
+
+# 익명게시판 글쓰기 완료시 작동
+@app.route('/article/unknown', methods=['POST'])
+def post_articles():
+    article_title = request.form['title_input']
+    article_content = request.form['content_input']
+    now = datetime.datetime.dnow()
+    article_created_at = now.strftime('%m-%d %H:%M')  # 시간 : 월-일 시간-분
+    article_view = 0
+    article_like = 0
+    article_is_secret = True
+    article_user_id = get_jwt_identity()
+
+    db.articles.insert_one({'article_title': article_title, 'article_content': article_content,
+                         'article_created_at': article_created_at,
+                         'article_view': article_view, 'article_like': article_like,
+                         'article_is_secret': article_is_secret,
+                         'article_user_id': article_user_id})
+
+
+    # article_writer = "{}기 {}".format(user['user_ordinal'],user['user_name'])
+
+
+
+@app.route('/article/known', methods=['GET'])
+def get_articles():
+
 
 
 if __name__ == '__main__':
